720ca5e9835bc57d2c8f3a8241acb5083094b791
Victor <100317843+savens-spec@users.noreply.github.com>
2022-11-08 03:15:28 +0100

    specifers

---
+++ formart_specifiyer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+// formart specifers %= defines and formats a type of data type to be displayed
+
+// %c = character
+// %s = string (array of characters)
+// %f = float
+// %lf = double
+// $d = interger
+
+// %.1 = decimal percition
+// %1 = minimum field width
+// %- = left align
+
+    return 0;
+}
